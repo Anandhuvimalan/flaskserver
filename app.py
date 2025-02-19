7fc806b01ec6abaae9602efe447191e1b4b908fb
Anandhu Vimalan <88484102+Anandhuvimalan@users.noreply.github.com>
2025-02-20 03:42:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -357,4 +357,5 @@ def setuppage():
     return render_template("setuppage.html", api_key=API_KEY)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
